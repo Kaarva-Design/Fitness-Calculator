49ba661a8131f210d07d8ab578b8a0df73cefffb
rohanharikr <rohan@kaarva.com>
2020-03-14 15:23:41 +0530

    ver_1

--- quiz.js
+++ quiz.js
@@ -186,7 +186,10 @@ restart = () => {
 
 }
 
-
+fillForm = () => {
+    document.getElementById("re").style.display = "none";
+    document.getElementById("form").style.display = "block";
+}
 
 // progressbar.js@1.0.0 version is used
 // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
